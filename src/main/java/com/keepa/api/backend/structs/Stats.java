c233206ec257ee78ae419e888de37db97f69c1b0
keepacom <info@keepa.com>
2018-07-22 10:06:44 +0200

    added stats fields

--- src/main/java/com/keepa/api/backend/structs/Stats.java
+++ src/main/java/com/keepa/api/backend/structs/Stats.java
@@ -42,6 +42,22 @@ public class Stats {
 	public int[] avg90 = null;
 
 	/**
+	 * Contains the weighted mean for the last 180 days.<br>
+	 * <p>Uses {@link Product.CsvType} indexing.</p>
+	 * If no offer was available in the given interval or there is insufficient data it has the value -1.
+	 */
+	public int[] avg180 = null;
+
+	/**
+	 * Contains the prices registered at the start of the interval specified in the product request's stats parameter.<br>
+	 * First dimension uses {@link Product.CsvType} indexing <br>
+	 * Second dimension is either null, if there is no data available for the price type, or
+	 * an array of the size 2 with the first value being the time of the extreme point (in Keepa time minutes) and the second one the respective extreme value.<br>
+	 * Use {@link KeepaTime#keepaMinuteToUnixInMillis(int)} (long)} to get an uncompressed timestamp (Unix epoch time).
+	 */
+	public int[] atIntervalStart = null;
+
+	/**
 	 * Contains the lowest prices registered for this product. <br>
 	 * First dimension uses {@link Product.CsvType} indexing <br>
 	 * Second dimension is either null, if there is no data available for the price type, or
