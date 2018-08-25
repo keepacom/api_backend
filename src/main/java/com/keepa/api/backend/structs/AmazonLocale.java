0af2f88bcc2b92e95a1b3750c9a85d86921e19fd
keepacom <info@keepa.com>
2018-08-25 10:13:36 +0200

    added Amazon Australia to available locales.

--- src/main/java/com/keepa/api/backend/structs/AmazonLocale.java
+++ src/main/java/com/keepa/api/backend/structs/AmazonLocale.java
@@ -4,5 +4,5 @@ package com.keepa.api.backend.structs;
  * Amazon Locale Domain Enum
  */
 public enum AmazonLocale {
-	RESERVED, US, GB, DE, FR, JP, CA, CN, IT, ES, IN, MX, BR
+	RESERVED, US, GB, DE, FR, JP, CA, CN, IT, ES, IN, MX, BR, AU
 }
