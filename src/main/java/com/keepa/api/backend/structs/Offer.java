f166cf63b376c6f5618f54de0f16007a98ab8bbe
ryzanbui02 <ryzan.b@zendom.co.uk>
2025-05-07 17:36:17 +0700

    (refactor) (style) make variable 'code' in 'OfferCondition' final and re-align code

--- src/main/java/com/keepa/api/backend/structs/Offer.java
+++ src/main/java/com/keepa/api/backend/structs/Offer.java
@@ -19,220 +19,220 @@ import static com.keepa.api.backend.helper.Utility.gson;
  */
 public class Offer {
 
-	/**
-	 * Unique id of this offer (in the scope of the product).
-	 * Not related to the offerIds used by Amazon, as those are user specific and only valid for a short time.
-	 * The offerId can be used to identify the same offers throughout requests.
-	 * <p>
-	 * Example: 4
-	 */
-	public int offerId;
+    /**
+     * Unique id of this offer (in the scope of the product).
+     * Not related to the offerIds used by Amazon, as those are user specific and only valid for a short time.
+     * The offerId can be used to identify the same offers throughout requests.
+     * <p>
+     * Example: 4
+     */
+    public int offerId;
 
-	/**
-	 * States the last time we have seen (and updated) this offer, in Keepa Time minutes.
-	 * <p>
-	 * Example: 2700145
-	 */
-	public int lastSeen = 0;
+    /**
+     * States the last time we have seen (and updated) this offer, in Keepa Time minutes.
+     * <p>
+     * Example: 2700145
+     */
+    public int lastSeen = 0;
 
-	/**
-	 * The seller id of the merchant.
-	 * <p>
-	 * Example: A2L77EE7U53NWQ (Amazon.com Warehouse Deals)
-	 */
-	public String sellerId = null;
+    /**
+     * The seller id of the merchant.
+     * <p>
+     * Example: A2L77EE7U53NWQ (Amazon.com Warehouse Deals)
+     */
+    public String sellerId = null;
 
-	/**
-	 * Contains the current price and shipping costs of the offer as well as, if available, the offer's history.
-	 * It has the format Keepa time minutes, price, shipping cost, [...].
-	 * <p>
-	 * The price and shipping cost are integers of the respective Amazon locale's smallest currency unit (e.g. euro cents or yen).
-	 * If we were unable to determine the price or shipping cost they have the value -2.
-	 * Free shipping has the shipping cost of 0.
-	 * If an offer is not shippable or has unspecified shipping costs the shipping cost will be -1.
-	 * To get the newest price and shipping cost access the last two entries of the array.<br>
-	 * Most recent price: offerCSV[offerCSV.length - 2]<br>
-	 * Most recent shipping cost: offerCSV[offerCSV.length - 1]
-	 */
-	public int[] offerCSV = null;
+    /**
+     * Contains the current price and shipping costs of the offer as well as, if available, the offer's history.
+     * It has the format Keepa time minutes, price, shipping cost, [...].
+     * <p>
+     * The price and shipping cost are integers of the respective Amazon locale's smallest currency unit (e.g. euro cents or yen).
+     * If we were unable to determine the price or shipping cost they have the value -2.
+     * Free shipping has the shipping cost of 0.
+     * If an offer is not shippable or has unspecified shipping costs the shipping cost will be -1.
+     * To get the newest price and shipping cost access the last two entries of the array.<br>
+     * Most recent price: offerCSV[offerCSV.length - 2]<br>
+     * Most recent shipping cost: offerCSV[offerCSV.length - 1]
+     */
+    public int[] offerCSV = null;
 
-	/**
-	 * The {@link OfferCondition} condition of the offered product. Integer value:
-	 * <br>0 - Unknown: We were unable to determine the condition.
-	 * <br>1 - New
-	 * <br> 2 - Used - Like New
-	 * <br>3 - Used - Very Good
-	 * <br>4 - Used - Good
-	 * <br>5 - Used - Acceptable
-	 * <br>6 - Refurbished
-	 * <br>7 - Collectible - Like New
-	 * <br>8 - Collectible - Very Good
-	 * <br>9 - Collectible - Good
-	 * <br>10 - Collectible - Acceptable
-	 * <br>Note: Open Box conditions will be coded as Used conditions.
-	 */
-	public byte condition = 0;
+    /**
+     * The {@link OfferCondition} condition of the offered product. Integer value:
+     * <br>0 - Unknown: We were unable to determine the condition.
+     * <br>1 - New
+     * <br> 2 - Used - Like New
+     * <br>3 - Used - Very Good
+     * <br>4 - Used - Good
+     * <br>5 - Used - Acceptable
+     * <br>6 - Refurbished
+     * <br>7 - Collectible - Like New
+     * <br>8 - Collectible - Very Good
+     * <br>9 - Collectible - Good
+     * <br>10 - Collectible - Acceptable
+     * <br>Note: Open Box conditions will be coded as Used conditions.
+     */
+    public byte condition = 0;
 
-	/**
-	 * The describing text of the condition.
-	 * <p>
-	 * Example: The item may come repackaged. Small cosmetic imperfection on top, [...]
-	 */
-	public String conditionComment = null;
+    /**
+     * The describing text of the condition.
+     * <p>
+     * Example: The item may come repackaged. Small cosmetic imperfection on top, [...]
+     */
+    public String conditionComment = null;
 
-	/**
-	 * Whether or not this offer is available via Prime shipping. Can be used as a FBA ("Fulfillment by Amazon") indicator as well.
-	 */
-	public boolean isPrime;
+    /**
+     * Whether this offer is available via Prime shipping. Can be used as a FBA ("Fulfillment by Amazon") indicator as well.
+     */
+    public boolean isPrime;
 
-	/**
-	 * If the price of this offer is hidden on Amazon due to a MAP ("minimum advertised price") restriction.
-	 * Even if so, the offer object will contain the price and shipping costs.
-	 */
-	public boolean isMAP;
+    /**
+     * If the price of this offer is hidden on Amazon due to a MAP ("minimum advertised price") restriction.
+     * Even if so, the offer object will contain the price and shipping costs.
+     */
+    public boolean isMAP;
 
-	/**
-	 * Indicating whether or not the offer is currently shippable.
-	 * If not this could mean for example that it is temporarily out of stock or a pre-order.
-	 */
-	public boolean isShippable;
+    /**
+     * Indicating whether the offer is currently shippable.
+     * If not this could mean for example that it is temporarily out of stock or a pre-order.
+     */
+    public boolean isShippable;
 
-	/**
-	 * Indicating whether or not the offer is an Add-on item.
-	 */
-	public boolean isAddonItem;
+    /**
+     * Indicating whether the offer is an Add-on item.
+     */
+    public boolean isAddonItem;
 
-	/**
-	 * Indicating whether or not the offer is a pre-order.
-	 */
-	public boolean isPreorder;
+    /**
+     * Indicating whether the offer is a pre-order.
+     */
+    public boolean isPreorder;
 
-	/**
-	 * Indicating whether or not the offer is a Warehouse Deal.
-	 */
-	public boolean isWarehouseDeal;
+    /**
+     * Indicating whether the offer is a Warehouse Deal.
+     */
+    public boolean isWarehouseDeal;
 
-	/**
-	 * Indicating whether or not our system identified that the offering merchant attempts to scam users.
-	 */
-	public boolean isScam;
+    /**
+     * Indicating whether our system identified that the offering merchant attempts to scam users.
+     */
+    public boolean isScam;
 
-	/**
-	 * Indicating whether or not the offer ships from China.
-	 */
-	public boolean shipsFromChina;
+    /**
+     * Indicating whether the offer ships from China.
+     */
+    public boolean shipsFromChina;
 
-	/**
-	 * True if the seller is Amazon (e.g. "Amazon.com").
-	 * <p>
-	 * Note: Amazon's Warehouse Deals seller account or other accounts Amazon is maintaining under a different name are not considered to be Amazon.
-	 */
-	public boolean isAmazon;
+    /**
+     * True if the seller is Amazon (e.g. "Amazon.com").
+     * <p>
+     * Note: Amazon's Warehouse Deals seller account or other accounts Amazon is maintaining under a different name are not considered to be Amazon.
+     */
+    public boolean isAmazon;
 
-	/**
-	 * Whether or not this offer is fulfilled by Amazon.
-	 */
-	public boolean isFBA;
+    /**
+     * Whether this offer is fulfilled by Amazon.
+     */
+    public boolean isFBA;
 
-	/**
-	 * This offer has a discounted Prime exclusive price. A Prime exclusive offer can only be ordered if the buyer has an active Prime subscription.
-	 */
-	public boolean isPrimeExcl;
-
-
-	/**
-	 * Contains the Prime exclusive price history of this offer, if available. A Prime exclusive offer can only be ordered if the buyer has an active Prime subscription.
-	 * It has the format Keepa time minutes, price, [...].
-	 * <p>
-	 * Most recent Prime exclusive price: primeExclCSV[primeExclCSV.length - 1]
-	 */
-	public int[] primeExclCSV;
-
-	/**
-	 * Contains the available stock of this offer as well as, if available, the stock's history.
-	 * It has the format Keepa time minutes, stock, [...].
-	 * <p>
-	 * Most recent stock: stockCSV[stockCSV.length - 1]
-	 */
-	public int[] stockCSV;
-
-	/**
-	 * Minimum order quantity. 0 if unknown.
-	 */
-	public int minOrderQty;
+    /**
+     * This offer has a discounted Prime exclusive price. A Prime exclusive offer can only be ordered if the buyer has an active Prime subscription.
+     */
+    public boolean isPrimeExcl;
 
 
-	/**
-	 * Contains one-time coupon details of this offer. Undefined if none is available.
-	 * Positive integer for an absolute discount or negative for a percentage discount.
-	 * Example:
-	 * 500 - Coupon with a $5 discount.
-	 * -15 - Coupon with a 15% discount.
-	 */
-	public int coupon;
+    /**
+     * Contains the Prime exclusive price history of this offer, if available. A Prime exclusive offer can only be ordered if the buyer has an active Prime subscription.
+     * It has the format Keepa time minutes, price, [...].
+     * <p>
+     * Most recent Prime exclusive price: primeExclCSV[primeExclCSV.length - 1]
+     */
+    public int[] primeExclCSV;
 
-	/**
-	 * Contains the coupon history of this offer, if available.
-	 * It has the format Keepa time minutes, coupon, [...].
-	 */
-	public int[] couponHistory;
+    /**
+     * Contains the available stock of this offer as well as, if available, the stock's history.
+     * It has the format Keepa time minutes, stock, [...].
+     * <p>
+     * Most recent stock: stockCSV[stockCSV.length - 1]
+     */
+    public int[] stockCSV;
+
+    /**
+     * Minimum order quantity. 0 if unknown.
+     */
+    public int minOrderQty;
 
 
-public enum OfferCondition {
-		UNKNOWN(0),
-		NEW(1),
-		USED_NEW(2), USED_VERY_GOOD(3), USED_GOOD(4), USED_ACCEPTABLE(5),
-		REFURBISHED(6),
-		COLLECTIBLE_NEW(7), COLLECTIBLE_VERY_GOOD(8), COLLECTIBLE_GOOD(9), COLLECTIBLE_ACCEPTABLE(10);
+    /**
+     * Contains one-time coupon details of this offer. Undefined if none is available.
+     * Positive integer for an absolute discount or negative for a percentage discount.
+     * Example:
+     * 500 - Coupon with a $5 discount.
+     * -15 - Coupon with a 15% discount.
+     */
+    public int coupon;
 
-		public byte code;
-		public static final OfferCondition[] values = OfferCondition.values();
+    /**
+     * Contains the coupon history of this offer, if available.
+     * It has the format Keepa time minutes, coupon, [...].
+     */
+    public int[] couponHistory;
 
-		public static Product.CsvType getCorrespondingCsvType(OfferCondition oc) {
-			Product.CsvType type = null;
-			switch (oc) {
-				case UNKNOWN:
-				case NEW:
-					break;
-				case USED_NEW:
-					type = Product.CsvType.USED_NEW_SHIPPING;
-					break;
-				case USED_VERY_GOOD:
-					type = Product.CsvType.USED_VERY_GOOD_SHIPPING;
-					break;
-				case USED_GOOD:
-					type = Product.CsvType.USED_GOOD_SHIPPING;
-					break;
-				case USED_ACCEPTABLE:
-					type = Product.CsvType.USED_ACCEPTABLE_SHIPPING;
-					break;
-				case REFURBISHED:
-					type = Product.CsvType.REFURBISHED_SHIPPING;
-					break;
-				case COLLECTIBLE_NEW:
-					type = Product.CsvType.COLLECTIBLE_NEW_SHIPPING;
-					break;
-				case COLLECTIBLE_VERY_GOOD:
-					type = Product.CsvType.COLLECTIBLE_VERY_GOOD_SHIPPING;
-					break;
-				case COLLECTIBLE_GOOD:
-					type = Product.CsvType.COLLECTIBLE_GOOD_SHIPPING;
-					break;
-				case COLLECTIBLE_ACCEPTABLE:
-					type = Product.CsvType.COLLECTIBLE_ACCEPTABLE_SHIPPING;
-					break;
-			}
 
-			return type;
-		}
+    public enum OfferCondition {
+        UNKNOWN(0),
+        NEW(1),
+        USED_NEW(2), USED_VERY_GOOD(3), USED_GOOD(4), USED_ACCEPTABLE(5),
+        REFURBISHED(6),
+        COLLECTIBLE_NEW(7), COLLECTIBLE_VERY_GOOD(8), COLLECTIBLE_GOOD(9), COLLECTIBLE_ACCEPTABLE(10);
 
-		OfferCondition(int index) {
-			code = (byte) index;
-		}
-	}
+        public final byte code;
+        public static final OfferCondition[] values = OfferCondition.values();
 
-	@Override
-	public String toString() {
-		return gson.toJson(this);
-	}
+        public static Product.CsvType getCorrespondingCsvType(OfferCondition oc) {
+            Product.CsvType type = null;
+            switch (oc) {
+                case UNKNOWN:
+                case NEW:
+                    break;
+                case USED_NEW:
+                    type = Product.CsvType.USED_NEW_SHIPPING;
+                    break;
+                case USED_VERY_GOOD:
+                    type = Product.CsvType.USED_VERY_GOOD_SHIPPING;
+                    break;
+                case USED_GOOD:
+                    type = Product.CsvType.USED_GOOD_SHIPPING;
+                    break;
+                case USED_ACCEPTABLE:
+                    type = Product.CsvType.USED_ACCEPTABLE_SHIPPING;
+                    break;
+                case REFURBISHED:
+                    type = Product.CsvType.REFURBISHED_SHIPPING;
+                    break;
+                case COLLECTIBLE_NEW:
+                    type = Product.CsvType.COLLECTIBLE_NEW_SHIPPING;
+                    break;
+                case COLLECTIBLE_VERY_GOOD:
+                    type = Product.CsvType.COLLECTIBLE_VERY_GOOD_SHIPPING;
+                    break;
+                case COLLECTIBLE_GOOD:
+                    type = Product.CsvType.COLLECTIBLE_GOOD_SHIPPING;
+                    break;
+                case COLLECTIBLE_ACCEPTABLE:
+                    type = Product.CsvType.COLLECTIBLE_ACCEPTABLE_SHIPPING;
+                    break;
+            }
+
+            return type;
+        }
+
+        OfferCondition(int index) {
+            code = (byte) index;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return gson.toJson(this);
+    }
 }
